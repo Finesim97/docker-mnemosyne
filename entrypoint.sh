67c37c75b8cc77ff87c0f42855a211568d81e5e4
Lukas Jansen <lukas.jansen1997@yahoo.de>
2019-10-29 13:44:29 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -e
 
-echo "UPDATE config SET value='$USERNAME' WHERE key='remote_access_username'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
-echo "UPDATE config SET value='$PASSWORD' WHERE key='remote_access_password'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+if [ -z ${USERNAME+x} ] || [ -z ${PASSWORD+x} ] ;  then 
+    echo "You have to provide a username(env: USERNAME) and password (env: PASSWORD)"
+    exit 1
+fi
+
+echo "UPDATE config SET value='''$USERNAME''' WHERE key='remote_access_username'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+echo "UPDATE config SET value='''$PASSWORD''' WHERE key='remote_access_password'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+
+unset USERNAME
+unset PASSWORD
 
 if [ "$1" = 'mnemosyne' ]; then
-    if [ -z ${USERNAME+x} ] || [ -z ${PASSWORD+x} ] ;  then 
-        echo "You have to provide a username(env: USERNAME) and password (env: PASSWORD)"
-        exit 1
-    fi
-    unset USERNAME
-    unset PASSWORD
-    mnemosyne --sync-server --web-server
+    exec mnemosyne --sync-server --web-server
 else
-    unset USERNAME
-    unset PASSWORD
     exec "$@"
 fi
