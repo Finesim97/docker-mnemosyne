75053f5667a779f216e08c69fe1fb35a1d1dae0b
Lukas Jansen <lukas.jansen1997@yahoo.de>
2019-10-29 11:42:57 +0100

    = not ==

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ if [ "$1" = 'mnemosyne' ]; then
         echo "You have to provide a username(env: USERNAME) and password (env: PASSWORD)"
         exit 1
     fi
-    echo "UPDATE config SET value='$USERNAME' WHERE key==remote_access_username" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
-    echo "UPDATE config SET value='$PASSWORD' WHERE key==remote_access_password" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
-    echo "UPDATE config SET value='True' WHERE key==run_sync_server" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
-    echo "UPDATE config SET value='${WEBSERVER:-False}' WHERE key==run_web_server" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+    echo "UPDATE config SET value='$USERNAME' WHERE key='remote_access_username'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+    echo "UPDATE config SET value='$PASSWORD' WHERE key='remote_access_password'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+    echo "UPDATE config SET value='True' WHERE key='run_sync_server" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
+    echo "UPDATE config SET value='${WEBSERVER:-False}' WHERE key='run_web_server'" | sqlite3 /home/mnemosyne/.config/mnemosyne/config.db
     unset USERNAME
     unset PASSWORD
     exec mnemosyne "$@"
